c4a27c2185ee04b7d741053b2a9110de59c9c9bf
kxdiii <alexfj-789@hotmail.com>
2026-01-14 21:54:23 -0500

    Implementando pantalla de registro

---
+++ views/signup/signup.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("signupForm")
+
+  if (form) {
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault()
+
+      const formData = new FormData(form)
+      const nombre = formData.get("nombre")
+      const email = formData.get("email")
+      const username = formData.get("username")
+      const password = formData.get("password")
+
+      try {
+        const response = await fetch(
+          "http://localhost/notas-app/backend/api/index.php/register",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ nombre, email, username, password }),
+          }
+        )
+
+        const data = await response.json()
+        console.log("Response data:", data)
+
+        if (data.success) {
+          console.log(data.message)
+          /* sessionStorage.setItem("user_id", data.user.id_usuario) */
+          /* window.location.href = "/notas-app/views/dashboard/login.html" */
+        } else {
+          console.log("Error: ", data.message)
+          /* alert("Error: " + data.message); */
+        }
+      } catch (error) {
+        console.error("Fetch error:", error)
+        /* alert("Error de conexión"); */
+      }
+    })
+  }
+})
